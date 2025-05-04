2109771252c758c2a8f70df4b73f99790c14259f
1520478@senati.pe <yassercbf@gmail.com>
2025-05-04 16:03:35 +0000

    agregando las datos correspondientes

--- pet-care-royal/src/firebase.js
+++ pet-care-royal/src/firebase.js
@@ -3,12 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  apiKey: "AIzaSyDrUkg21-p81haD-5qu1cNjy3fyaucAank",
   authDomain: "pet-care-royal.firebaseapp.com",
   projectId: "pet-care-royal",
-  storageBucket: "pet-care-royal.appspot.com",
-  messagingSenderId: "1234567890",
-  appId: "1:1234567890:web:xxxxxxxxxxxxxxxx"
+  storageBucket: "pet-care-royal.firebasestorage.app",
+  messagingSenderId: "13005705063",
+  appId: "1:13005705063:web:98d82c682f65535ddce9c2",
+  measurementId: "G-PSFJC1HLHN"
 };
 
 const app = initializeApp(firebaseConfig);
